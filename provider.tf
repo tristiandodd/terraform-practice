4751fb22e781714f32ba8d6999c694763f7c6459
Tristian Dodd <tristiandodd@gmail.com>
2018-07-17 10:47:52 -0500

    last push for vpc template

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ provider "aws" { //Must have the provider, and for aws the region you want it in
 }
 
 resource "aws_vpc" "main" { // type of resource first and then the name of it.
-  cidr_block       ="180.160.0.0/16" //Cidr Block for iP Range
+  cidr_block       ="190.160.0.0/16" //Cidr Block for iP Range
   instance_tenancy = "default" //Type of instance can be default, dedicated or host
   tags {
     Name="main"   //Same as the Tags you use in AWS console to label things
